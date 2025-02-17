cfb13acb6d1a8a3873da6df03662b19d18b1a3db
mishiko222 <mishikototladze14@gmail.com>
2025-02-17 23:19:00 +0400

    second commit

---
+++ day02/homework/lesson3.py
@@ -0,0 +1,28 @@
+# სტრინგი შექმნა
+ 
+string = "123"
+ 
+integer = int(string)
+
+
+
+print("სტრინგი:", string)
+print("ინტეჯერი:", integer)
+
+
+
+
+
+
+heloo = "გამარჯობა"
+print(heloo)
+
+element = ("ელემენტი") * 10
+print(element.strip())
+
+number = 500 * 2
+print(number)
+
+
+bollean = 5 + 3 < 2
+print(bollean)
---
+++ day03/classwork/cw.py
@@ -0,0 +1,36 @@
+#N1
+
+num1 = 10
+num2 = 15
+num3 = 25
+num4 = int(num1) * int(num2) * int(num3)
+print(num4)
+
+
+#N2
+saxeli = input("miutite sheni saxeli: ")
+gvari = input("miutite sheni gvari: ")
+full_name = saxeli + " " + gvari
+print(full_name)
+
+
+#N3
+num1 = float(input("შეიყვანეთ პირველი რიცხვი: "))
+num2 = float(input("შეიყვანეთ მეორე რიცხვი: "))
+
+
+print(num1 > num2)
+
+
+#N4
+#შევიტანოთ სიტყვა და რიცხვი
+სიტყვა = input("შეიყვანეთ სიტყვა: ")
+n = int(input("შეიყვანეთ რიცხვი: "))
+
+#გამოვიტანოთ სიტყვა n-ჯერ
+
+print(სიტყვა * n) 
+
+
+
+
